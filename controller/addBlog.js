f66a12f84db900415220ebb16581d8dcafe00425
HafsaR <hafsa34343@gmail.com>
2020-04-11 14:23:06 -0600

    crud

--- controller/addBlog.js
+++ controller/addBlog.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const urlencodedparser = bodyParser.urlencoded({ extended: false });
 const mongoose = require("mongoose");
 
+const ObjectID = require("mongodb").ObjectID;
+
 const blogModel = require("../models/blog.model").blogModel;
 const adminModel = require("../models/blog.model").adminModel;
 
@@ -27,26 +29,41 @@ router.get("/addBlog/", (req, res) => {
   res.render("backend/addBlog", { title: "Add Blog" });
 });
 
-router.post("/addBlog", (req, res) => {
-  let data = req.body;
-  console.log(data);
-  blogModel.create(data, (err, small) => {
-    if (err) {
-      console.log("error insertion record", err);
-    } else {
-      console.log("record inserted");
-      res.status(200).json(small);
+router.post("/addBlog/", (req, res) => {
+  insertRecord(req, res);
+});
+router.post("/addBlog/:id", (req, res) => {
+  updateRecord(req, res);
+});
+router.get("/addBlog/:id", (req, res) => {
+  let selectedId = req.params.id;
+  let filter = { _id: ObjectID(selectedId) };
+  blogModel.findById(filter, (err, doc) => {
+    if (!err) {
+      res.render("backend/addBlog", {
+        blogModel: doc,
+      });
     }
   });
 });
+
 router.get("/listBlog", (req, res) => {
-  res.render("backend/listBlog", { title: "Blog Info" });
+  blogModel.find((err, docs) => {
+    if (!err) {
+      res.render("backend/listBlog", {
+        list: docs,
+      });
+    } else {
+      console.log("Error in retrieving employee list :" + err);
+    }
+  });
 });
 
 router.post("/adminLogin", urlencodedparser, (req, res) => {
   var data = req.body;
   adminModel.create(data, (err, doc) => {
-    if (!err) {      console.log("record inserted");
+    if (!err) {
+      console.log("record inserted");
       res.status(200).json(doc);
     } else console.log("Errors during record insertion : " + err);
   });
@@ -62,5 +79,52 @@ router.get("/blogSingle", (req, res) => {
 router.get("/blogGrid", (req, res) => {
   res.render("blogGrid", { title: "All blogs" });
 });
+
+router.get("/listBlog/delete/:id", (req, res) => {
+  let selectedId = req.params.id;
+  let filter = { _id: ObjectID(selectedId) };
+  blogModel.deleteOne(filter, (err, result) => {
+    if (err) {
+      console.log("Error during record delete ", err);
+    } else {
+      console.log("Record deleted");
+      res.redirect("/listBlog");
+    }
+  });
+});
+
+function insertRecord(req, res) {
+  let data = req.body;
+  blogModel.create(data, (err, doc) => {
+    if (!err) {
+      console.log("data inserted");
+      res.redirect("/listBlog");
+      // res.render("backend/addBlog", {
+      //   blogModel: req.body,
+      // });
+    } else {
+      console.log("error during data insert" + err);
+    }
+  });
+}
+
+function updateRecord(req, res) {
+  let selectedId = req.params.id;
+  let filterId = { _id: ObjectID(selectedId) };
+  //let id = req.body._id;
+  let data = req.body;
+  let update = { $set: data };
+  blogModel.updateOne(filterId, update, (err, result) => {
+    if (err) {
+      console.log("Error update info" + err);
+    } else {
+      console.log("record updated");
+       res.redirect("/listBlog");
+      // res.render("backend/listBlog", {
+      //   blogModel:result,
+      // });
+    }
+  });
+}
 
 module.exports = router;
--- models/db.js
+++ models/db.js
@@ -1,18 +1,34 @@
-const MongoClient = require("mongodb").MongoClient;
-const uri =
-  "mongodb+srv://mongo:mongo@cluster0-xq7df.mongodb.net/BlogDB?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-client.connect((err) => {
-  if (!err) {
-    console.log("database connected");
+// const MongoClient = require("mongodb").MongoClient;
+// const uri =
+//   "mongodb+srv://mongo:mongo@cluster0-xq7df.mongodb.net/BlogDB?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// client.connect((err) => {
+//   if (!err) {
+//     console.log("database connected");
 
-    // perform actions on the collection object
-    // client.close();
-  } else {
-    console.log("Error connectind Database" + err);
+//     // perform actions on the collection object
+//     // client.close();
+//   } else {
+//     console.log("Error connectind Database" + err);
+//   }
+// });
+// require("./blog.model");
+
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb://localhost:27017/BlogDB",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) {
+      console.log("MongoDB Connection Succeeded.");
+    } else {
+      console.log("Error in DB connection : " + err);
+    }
   }
-});
+);
+
 require("./blog.model");
