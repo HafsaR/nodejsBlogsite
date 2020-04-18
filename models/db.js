43a5df03f90c4f559e3c203dc9d59350602aa007
HafsaR <hafsa34343@gmail.com>
2020-04-18 13:38:42 -0600

    db

--- models/db.js
+++ models/db.js
@@ -1,34 +1,32 @@
-// const MongoClient = require("mongodb").MongoClient;
-// const uri =
-//   "mongodb+srv://mongo:mongo@cluster0-xq7df.mongodb.net/BlogDB?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-// client.connect((err) => {
-//   if (!err) {
-//     console.log("database connected");
+// const mongoose = require("mongoose");
 
-//     // perform actions on the collection object
-//     // client.close();
-//   } else {
-//     console.log("Error connectind Database" + err);
+// mongoose.connect(
+//   "mongodb://localhost:27017/BlogDB",
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   (err) => {
+//     if (!err) {
+//       console.log("MongoDB Connection Succeeded.");
+//     } else {
+//       console.log("Error in DB connection : " + err);
+//     }
 //   }
-// });
+// );
+
 // require("./blog.model");
 
-const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb://localhost:27017/BlogDB",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    if (!err) {
-      console.log("MongoDB Connection Succeeded.");
-    } else {
-      console.log("Error in DB connection : " + err);
-    }
-  }
-);
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config()
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect(process.env.MONGO_CONNECTION,{ useNewUrlParser: true } ,  err => {
+    if(!err){console.log('Database connection successful.');}
+    else{console.log('database connection error: ' + err);}
+});
 
 require("./blog.model");
